145d3933cb0a9cdb2669dbfbb5e21c4f9fe51469
stefano-malacrino <38613112+stefano-malacrino@users.noreply.github.com>
2019-04-05 10:48:35 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 PAD_CHAR = "**PAD**"
-EMBEDDING_SIZE = 100
-MAX_LENGTH = 2500
+EMBEDDING_SIZE = 300
+MAX_LENGTH = 5200
 
 #where you want to save any models you may train
-MODEL_DIR = '/users/engs2002/src/Oxford-Boella-NLP/reproduction/caml-mimic/saved_models/'
+MODEL_DIR = '/path/to/caml-mimic/saved_models/'
+DATA_DIR = '/path/to/caml-mimic/mimicdata/'
 
-DATA_DIR = '/users/engs2002/src/Oxford-Boella-NLP/reproduction/caml-mimic/mimicdata/'
-MIMIC_3_DIR = '/users/engs2002/src/Oxford-Boella-NLP/reproduction/caml-mimic/mimicdata/mimic3'
-MIMIC_2_DIR = '/users/engs2002/src/Oxford-Boella-NLP/reproduction/caml-mimic/mimicdata/mimic2'
+#MIMIC_3_DIR = '/path/to/caml-mimic/mimicdata/mimic3'
+#MIMIC_2_DIR = '/path/to/caml-mimic/mimicdata/mimic2'
